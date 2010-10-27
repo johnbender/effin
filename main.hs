f178bf5d62f83433ac45f60dbc19f9f5cee0682c
John Bender <john.m.bender@gmail.com>
2010-10-26 20:34:24 -0700

    functions only parsing and conversion to libffi

--- main.hs
+++ main.hs
@@ -11,83 +11,62 @@ import Data.Either
 import Control.Monad
 import Control.Monad.Writer
 
-data Object = Object Main.Name [CDecl]
+getAnalysed = do parseResult  <- parseCFile (newGCC "gcc") Nothing [] "libvirt.h"
+                 case parseResult of
+                   Right (CTranslUnit x _) -> printAttach $ filter typeDefs $ filter (byName "vir") $ grabFunctions x
+                   Left (ParseError (strings, _)) -> print $ head strings
+                 return ()
 
-data Member = Function Main.Name [Member]
-            | Property Main.Name Main.TypeName
+printAttach = print . (map stringed)
+    where stringed (return, name, args) = "attach_function " ++ ":" ++ head name ++ ", " ++ handleArgs ++ ", " ++ (head $ argsToString [(handleReturn (head return) args)])
+              where handleArgs | args == [] = "[]"
+                               | (head $ reverse args) /= "ptr" = "[" ++ (concat $ intersperse ", " $ argsToString args) ++ "]"
+                               | otherwise = "[" ++ (concat $ intersperse ", " $ argsToString $ reverse $ tail $ reverse args) ++ "]"
 
-type TypeName = String
-type Name = String
+argsToString = reverse . (toString [])
+    where toString acc [] = acc
+          toString acc (x:xs) | "* *" `isInfixOf` x = toString (":pointer":acc) xs
+                              | "unsigned char *" `isInfixOf` x = toString (":pointer":acc) xs
+                              | "char *" `isInfixOf` x  = toString (":string":acc) xs
+                              | "void *" `isInfixOf` x = toString (":void_pointer":acc) xs
+                              | "int" `isInfixOf` x  = toString (":int":acc) xs
+                              | "unsigned long long" `isInfixOf` x = toString ( ":ulong_long":acc) xs
+                              | "unsigned long" `isInfixOf` x = toString ( ":ulong":acc) xs
+                              | otherwise = toString ((":" ++ (head $ words x)):acc) xs
 
-class Named a where
-    name :: a -> Main.Name
+handleReturn return [] = return
+handleReturn return args | "ptr" == (head $ reverse args) = return ++ " *"
+                         | otherwise = return
 
-instance Named Object where
-    name (Object x _ ) = x
+namesOnly (_, [], _) = ""
+namesOnly (_, foo, _) = head foo
 
-instance Named Member where
-    name (Function x _) = x
-    name (Property x _) = x
+byName _ (_, [], _) = False
+byName name (_, names, _) =  name `isInfixOf` (show $ head names)
 
-instance Show Object where
-    show (Object name decls) | name /= "" = "Object (" ++ name ++ " [" ++ ( concat $ map (show . pretty) decls) ++ "]" ++ ")"
-                             | otherwise = ""
+typeDefs (defs, _, _) = not ("typedef" `elem` defs)
 
-main = print 'x'
-
-getAnalysed = do Right (CTranslUnit x _) <- parseCFile (newGCC "gcc") Nothing [] "foo.h"
-                 mapM (print) $ grabFunctions x
---                 mapM print $ catMaybes $ record x
-    -- where record = (mapM print) . (grabPropertyNames . grabStructMembers . grabStructs)
-
-
-record :: [CExtDecl] -> [Maybe Object]
-record = grabStructs
-
-grabFunctions = (map ident) . concat . funcDeclr
-    where ident (CDeclr (Just (Ident id _ _)) _ _ _ _) = id
-
-grabStructs = (map nameAndDecls) . structs . concat . typeSpecs
-    where nameAndDecls (CStruct _ (Just (Ident id _ _)) (Just decls) _  _ ) = Just (Object id decls)
-          nameAndDecls _ = Nothing
+grabFunctions = funcDeclr
 
 funcDeclr [] = []
 funcDeclr (x:xs) = (funcDeclr' x) : (funcDeclr xs)
-    where funcDeclr' (CDeclExt (CDecl _ declrs _)) = catMaybes $ map (\(d, _, _) -> d) declrs
+    where funcDeclr' (CDeclExt (CDecl declSpecs declrs _)) = (typeSpec declSpecs, grabDeclrsPart ident declrs, concat $ grabDeclrsPart derivedFunction declrs)
+
+grabDeclrsPart f =  (map f) . catMaybes . map (\(d, _, _) -> d)
+
+typeSpec = map (show . pretty)
+
+ident (CDeclr (Just (Ident id _ _)) _ _ _ _) = id
+
+derivedFunction (CDeclr _ declarations _ _ _) = funcDeclrIdents
+    where funcDeclrIdents = concat $ map extract declarations
+              where extract (CFunDeclr (Left idents) _ _) = map identToString idents
+                    extract (CFunDeclr (Right (c, b)) _ _) = (map (show . pretty) c)
+                    extract (CPtrDeclr typeQ node ) = ["ptr"]
+                    extract (CArrDeclr _ _ _ ) = ["arr"]
+
 
 typeSpecs [] = []
 typeSpecs (x:xs) = (typeSpecs' x) : (typeSpecs xs)
    where typeSpecs' (CDeclExt (CDecl specs _ _)) = (\( _ , _ , _ , specs , _ ) -> specs) $ partitionDeclSpecs specs
 
-functions [] = []
-functions ((CSUType y _):xs) = y : structs xs
-functions (x:xs) = structs xs
-
-structs [] = []
-structs ((CSUType y _):xs) = y : structs xs
-structs (x:xs) = structs xs
-
-structName (CStruct _ (Just (Ident name  _ _ )) _ _ _) = name
-structName _ = ""
-
-structMemberDeclarations (CStruct _ _ decls _ _) = decls
-
-
-grabFunIdents = concat . (map fst) . rights . (map funDeclr)
-
-grabPropertyDeclarations = propertyFilter snd
-
---grabPropertyNames = tell . (filter (/= "")) . (propertyFilter fst)
-
-propertyFilter f = (map (f . (fromMaybe ("", [])) . properties))
-
-grabStructMembers = concat . (map cdeclToCdeclr) . concat . (map ((fromMaybe []) . structMemberDeclarations))
-    where cdeclToCdeclr (CDecl _ declList _ ) = map (\(x,_,_) -> x) declList
-
-
-properties (Just (CDeclr (Just (Ident name _ _ ) ) derived _ _ _ )) | length derived == 2 = Just (name, derived)
-                                                                    | otherwise = Nothing
-properties _ = Nothing
-
-funDeclr (CFunDeclr newOrOld _ _ ) = newOrOld
-funDeclr _ = Left []
